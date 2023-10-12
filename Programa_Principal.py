f1714a314e72fce4f8de4d6975cc0a77a387a8eb
Marcio O <marcio_ottoboni@hotmail.com>
2023-10-12 16:09:00 +0100

    Create Programa_Principal.py
    Primeiro Comit, apenas para versionamento

---
+++ Programa_Principal.py
@@ -0,0 +1,61 @@
+import Classses as  CL
+import tkinter as tk
+
+# Função chamada quando o botão "OK" é clicado
+def mostrar_informacoes():
+    cantores = entry_cantores.get().split(',')
+    estilos = entry_estilos.get().split()
+    
+    if sl_top10.get() == 1:
+        CL.BaixadorMusicas(cantores, estilos).Top_10_Musicas()
+    if sl_dico.get() == 1:
+        CL.BaixadorMusicas(cantores, estilos).Todas_Discografia()
+    if sl_parecidos.get() == 1:
+        CL.BaixadorMusicas(cantores, estilos).Top_10_Parecidos()
+        
+    if sl_parecidos.get() != 1 and sl_dico.get() != 1 and sl_top10.get() != 1:
+       print(f'Selecione alguma opcao!') 
+    
+# Configuração da janela
+root = tk.Tk()
+root.title("Download De Musicas")
+largura, altura, posicaoX,posicaoY  = 380,170,300,300
+root.geometry(f"{largura}x{altura}+{posicaoX}+{posicaoY}")
+
+# Título para Cantores
+label_cantores = tk.Label(root, text="       Cantor(s) separados por vírgula:")
+label_cantores.grid(row=0, column=0, columnspan=2)
+entry_cantores = tk.Entry(root)
+entry_cantores.grid(row=0, column=2, columnspan=2)
+
+#Linha de Espaco 
+label_separador_0 = tk.Label(root, text="")
+label_separador_0.grid(row=1, column=0, columnspan=2)
+
+# Título para Estilos
+label_estilos = tk.Label(root, text="Estilo(s) separado por vírgula:")
+label_estilos.grid(row=2, column=0, columnspan=2)
+entry_estilos = tk.Entry(root)
+entry_estilos.grid(row=2, column=2, columnspan=2)
+
+# Inicia os check button em branco
+sl_top10,sl_dico, sl_parecidos = tk.IntVar(),tk.IntVar(),tk.IntVar()
+
+ck_top10 = tk.Checkbutton(root, text="Top 10 músicas", variable=sl_top10)
+ck_disco = tk.Checkbutton(root, text="Toda a discografia", variable=sl_dico)
+ck_parecidos = tk.Checkbutton(root, text="Top 10 parecidos", variable=sl_parecidos)
+
+ck_top10.grid(row=3, column=0)
+ck_disco.grid(row=3, column=1)
+ck_parecidos.grid(row=3, column=2)
+
+#Linha de Espaco 
+label_separador = tk.Label(root, text="")
+label_separador.grid(row=4, column=0, columnspan=2)
+
+# Botão "OK"
+botao_ok = tk.Button(root, text="Baixar", command=mostrar_informacoes)
+botao_ok.grid(row=5, column=2, columnspan=4)
+
+# Iniciar a interface
+root.mainloop()
